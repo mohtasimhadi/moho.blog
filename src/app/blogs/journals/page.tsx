0af67193ddbb51795b555415af683473d7aa4fdc
Mohtasim Hadi Rafi <mohtasimhadi@gmail.com>
2025-05-07 19:39:13 -0500

    API separation

--- src/app/blogs/journals/page.tsx
+++ src/app/blogs/journals/page.tsx
@@ -1,30 +1,33 @@
-import { getDatabase, transformPost } from "@/lib/notion";
+"use client";
+
+import { useEffect, useState } from "react";
 import BlogCard from "@/app/components/BlogCard";
 import { BlogPost } from "@/types/blogs";
 
-export default async function BlogPage() {
-  const [posts] = await Promise.all([
-    getDatabase(process.env.NOTION_JOURNALS_DB_ID!),
-  ]);
+export default function BlogPage() {
+  const [posts, setPosts] = useState<BlogPost[]>([]);
 
-  const blogs = posts.map((post) =>
-    transformPost(post, "journals")
-  );
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('/api/getPosts');
+      if (response.ok) {
+        const blogs: BlogPost[] = await response.json();
+        setPosts(blogs);
+      } else {
+        console.error("Failed to fetch posts.");
+      }
+    };
 
-  // Sort each category separately
-  const sortByDate = (a: BlogPost, b: BlogPost) =>
-    new Date(b.date).getTime() - new Date(a.date).getTime();
-
-  blogs.sort(sortByDate);
+    fetchPosts();
+  }, []);
 
   return (
     <div className="flex flex-col md:flex-row gap-4">
       <div className="md:w-3/4 p-4">
-
         <div className="mb-8">
           <h2 className="text-2xl font-semibold mb-4">Journals</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-            {blogs.map((post) => (
+            {posts.map((post) => (
               <BlogCard key={post.id} post={post} />
             ))}
           </div>
